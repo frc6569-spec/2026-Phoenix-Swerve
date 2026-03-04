d6d7b0c36f1e9073bc1679651df369caf1dd55ad
bburrows76 <123828694+bburrows76@users.noreply.github.com>
2026-03-04 14:06:39 -0500

    updated and tuned code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,6 +28,7 @@ public class Robot extends TimedRobot {
     public void robotPeriodic() {
         m_timeAndJoystickReplay.update();
         CommandScheduler.getInstance().run(); 
+        m_robotContainer.periodic();
     }
 
     @Override
